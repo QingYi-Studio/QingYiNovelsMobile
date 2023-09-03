e6519138f56bf070c6147a6cd7e52d80cc3a1359
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-09-03 19:24:28 +0800

    完善加载与返回模块

--- app/src/main/java/com/qingyi/novels/MainActivity.kt
+++ app/src/main/java/com/qingyi/novels/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.qingyi.novels
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -12,9 +13,29 @@ class MainActivity : ComponentActivity() {
         val webView = WebView(this)
         webView.settings.userAgentString = "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36" // 设置浏览器 UA 为手机
         webView.settings.javaScriptEnabled = true // 允许执行 JavaScript
-        webView.webViewClient = WebViewClient()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                if (url.startsWith("http://") || url.startsWith("https://")) {
+                    view.loadUrl(url)
+                }
+                return true
+            }
+        }
 
         webView.loadUrl("https://gw-novels.zeabur.app/")
         setContentView(webView)
     }
-}
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val webView = WebView(this)
+            webView.settings.userAgentString = "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36" // 设置浏览器 UA 为手机
+            webView.settings.javaScriptEnabled = true // 允许执行 JavaScript
+            webView.loadUrl("https://gw-novels.zeabur.app/")
+            setContentView(webView)
+            }
+        return true
+        }
+        // return super.onKeyDown(keyCode, event)
+    }
