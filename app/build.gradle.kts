6c2405e8b2618292e4a19798c53e2f1fcbd0bc26
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-09-21 09:32:49 +0800

    图标修改，试图修改为圆角矩形

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
