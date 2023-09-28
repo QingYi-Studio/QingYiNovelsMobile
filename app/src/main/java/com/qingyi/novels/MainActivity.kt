9776dd8c94251c6f0fe4fe78c1c3e62805c135dd
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-09-28 15:17:58 +0800

    重构

--- app/src/main/java/com/qingyi/novels/MainActivity.kt
+++ app/src/main/java/com/qingyi/novels/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.qingyi.novels
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.webkit.ConsoleMessage
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
@@ -11,6 +15,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import java.io.*
 
 class MainActivity : ComponentActivity() {
 
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_page)
+        setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
 
@@ -79,5 +84,39 @@ class MainActivity : ComponentActivity() {
         }
 
         webView.loadUrl("https://qingyi-novels.zeabur.app/index.html")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    fun back(view: View) {
+        webView.clearHistory()
+        webView.loadUrl("https://qingyi-novels.zeabur.app/index.html")
+    }
+
+    fun good_author(view: View) {
+        webView.loadUrl("https://qingyi-novels.zeabur.app/excellent_author/index.html")
+    }
+
+    fun update(view: View) {
+        // webView.loadUrl("https://qingyi-novels.zeabur.app/download/index.html")
+
+        val link = "https://hub.yzuu.cf/Grey-Wind/QingYiNovelsMobile/releases/latest/download/app.apk"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(intent)
     }
 }
