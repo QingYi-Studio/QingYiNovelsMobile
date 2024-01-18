e9ed3630a043113d141b64e674260ef7f86a9a50
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-01-19 01:31:38 +0800

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.5.1"
+        versionName = "Release 1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
